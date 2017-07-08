924549721b23cc5a7cadd9feeb3d2c3621579204
Pablo FAUST <pfaust@e3r4p7.42.fr>
2017-07-09 00:52:33 +0200

    ex02 j03 termine

--- j03/ex02/ft_swap.c
+++ j03/ex02/ft_swap.c
@@ -6,35 +6,15 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 23:28:47 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/08 23:44:42 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/07/09 00:50:57 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	<unistd.h>
-
-void	ft_putchar(char c)
+void	ft_swap(int *a, int *b)
 {
-	write(1, &c, 1);
-}
+	int		x;
 
-int		main()
-{
-	int		a;
-	int		b;
-	int		*ptra;
-	int		*ptrb;
-	int		**ptra2;
-	int		**ptrb2;
-
-	a = 42;
-	b = 47;
-	ptra = &a;
-	ptrb = &b;
-	ptra2 = &ptra;
-	ptrb2 = &ptrb;
-	**ptrb2 = *ptra;
-	**ptra2 = *ptrb;
-	ft_putchar(a);
-	ft_putchar(b);
-	return(0);
+	x = *a;
+	*a = *b;
+	*b = x;
 }
