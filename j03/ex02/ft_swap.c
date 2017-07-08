a032daa6396a3054c990e7edda09597e71509024
Pablo FAUST <pfaust@e1r11p7.42.fr>
2017-07-08 23:48:51 +0200

    debut ex02 du j03

--- j03/ex02/ft_swap.c
+++ j03/ex02/ft_swap.c
@@ -1,16 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/07 16:29:33 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/08 23:27:01 by pfaust           ###   ########.fr       */
+/*   Created: 2017/07/08 23:28:47 by pfaust            #+#    #+#             */
+/*   Updated: 2017/07/08 23:44:42 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+#include	<unistd.h>
+
+void	ft_putchar(char c)
 {
-	*nbr = 42;
+	write(1, &c, 1);
+}
+
+int		main()
+{
+	int		a;
+	int		b;
+	int		*ptra;
+	int		*ptrb;
+	int		**ptra2;
+	int		**ptrb2;
+
+	a = 42;
+	b = 47;
+	ptra = &a;
+	ptrb = &b;
+	ptra2 = &ptra;
+	ptrb2 = &ptrb;
+	**ptrb2 = *ptra;
+	**ptra2 = *ptrb;
+	ft_putchar(a);
+	ft_putchar(b);
+	return(0);
 }
