6c09257ed45f891f9e6120b2f91e658930b40df9
Pablo FAUST <pfaust@e2r2p14.42.fr>
2017-07-08 16:01:53 +0200

    rush01 termine et norme

--- projet01/main.c
+++ projet01/main.c
@@ -5,69 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/06 14:22:28 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/08 03:09:25 by pfaust           ###   ########.fr       */
+/*   Created: 2017/07/08 15:26:23 by pfaust            #+#    #+#             */
+/*   Updated: 2017/07/08 15:59:49 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+void	ft_rush01(int x, int y);
 
-int		ft_nbr_col(int n)
+int		main(void)
 {
-	int nbr_col;
-
-	nbr_col = 0;
-	while (n != 0)
-	{
-		nbr_col++;
-		n = n - 1;
-	}
-	return (nbr_col);
-}
-
-int		ft_nbr_li(int n)
-{
-	int nbr_li;
-
-	nbr_li = 0;
-	while (n != 0)
-	{
-		nbr_li++;
-		n = n - 1;
-	}
-	return (nbr_li);
-}
-
-int		ft_rush01(int x, int y)
-{
-	int a;
-	int b;
-	int c;
-	int nbr_col;
-	int nbr_li;
-
-	a = 42;
-	b = 47;
-	c = 92;
-	nbr_col = ft_nbr_col(x);
-	nbr_li = ft_nbr_li(y);
-
-	while (nbr_li >= 0)
-	{
-		while (nbr_col >= 0)
-		{
-			ft_putchar(a);
-			nbr_col--;
-		}
-		ft_putchar('\n');
-		nbr_li --;
-		nbr_col = ft_nbr_col(x);
-	}
+	ft_rush01(30, 10);
 	return (0);
-}
-
-int 	main()
-{
-	ft_rush01(5, 5);
-	return(0);
 }
--- projet01/rush01.c
+++ projet01/rush01.c
@@ -1,36 +1,111 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test1.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/07 22:41:24 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/08 01:42:25 by pfaust           ###   ########.fr       */
+/*   Created: 2017/07/06 14:22:28 by pfaust            #+#    #+#             */
+/*   Updated: 2017/07/08 15:55:01 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
+void	ft_putnbr(int n);
 
-int		ft_rush01(int x)
+void	ft_ligne1(int n)
 {
-	int 	a;
-	int		nbr;
+	int		a;
 
-	nbr = 0;
-	a = 42;
-
-	ft_putchar(x);
-	while (x != 0)
+	a = 1;
+	while (a <= n)
 	{
-		nbr++;
-		x = x - 1;
+		if (a == 1)
+		{
+			ft_putchar(47);
+		}
+		else if (a == n)
+		{
+			ft_putchar(92);
+		}
+		else
+		{
+			ft_putchar(42);
+		}
+		a++;
 	}
+}
 
-	while (nbr <= x)
+void	ft_ligne2(int n)
+{
+	int		a;
+
+	a = 1;
+	while (a <= n)
 	{
-		ft_putchar(a);
-		nbr--;
+		if (a == 1)
+		{
+			ft_putchar(92);
+		}
+		else if (a == n)
+		{
+			ft_putchar(47);
+		}
+		else
+		{
+			ft_putchar(42);
+		}
+		a++;
 	}
-	return(0);
+}
+
+void	ft_ligne_inter(int n)
+{
+	int		a;
+
+	a = 1;
+	while (a <= n)
+	{
+		if (a == 1)
+		{
+			ft_putchar(42);
+		}
+		else if (a == n)
+		{
+			ft_putchar(42);
+		}
+		else
+		{
+			ft_putchar(' ');
+		}
+		a++;
+	}
+}
+
+int		ft_rush01(int x, int y)
+{
+	int		a;
+	int		b;
+
+	a = 1;
+	b = 1;
+	while (b <= y)
+	{
+		if (b == 1)
+		{
+			ft_ligne1(x);
+		}
+		else if (b == y)
+		{
+			ft_ligne2(x);
+		}
+		else
+		{
+			ft_ligne_inter(x);
+		}
+		a = 1;
+		b++;
+		ft_putchar('\n');
+	}
+	return (0);
 }
