f808750cda7cf7238b79beb592df0467fb5ef7e9
Pablo FAUST <pfaust@e3r3p1.42.fr>
2017-07-16 22:21:17 +0200

    dernier push - abandon

--- matchnmatch/ex00/main.c
+++ matchnmatch/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/15 08:37:21 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/15 12:43:53 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/07/16 20:34:21 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,63 @@ int		match(char *s1, char *s2);
 
 int		main()
 {
-	printf("%d\n", match("", "*"));
-	return (0);
+	printf("\nEXPECretTED 0\n");
+	printf("%d\n", match("est", "t") );
+	printf("%d\n", match("test", "**testt") );
+	printf("%d\n", match("test", "ts**") );
+	printf("%d\n", match("test", "**te") );
+	printf("%d\n", match("test", "t**s") );
+	printf("%d\n", match("test", "") );
+	printf("%d\n", match("", "test") );
+	printf("%d\n", match("abc", "abd") );
+	printf("%d\n", match("test", "es*") );
+	printf("%d\n", match("test", "t") );
+	printf("%d\n", match("salut ca va", "salutc*va") );
+	printf("%d\n", match("kayak", "k*aak") );
+	printf("%d\n", match("salut les amis", "salut les*mis ") );
+	printf("%d\n", match("caca boudin", "*i") );
+	printf("%d\n", match("*etoile*", "*etoile") );
+	printf("%d\n", match("*etole*", "*etoile*") );
+	printf("%d\n", match("*etoile*", "etoile*") );
+	printf("%d\n", match("*", "*l") );
+	printf("%d\n", match("ppa*", "ppa") );
+	printf("%d\n", match("bonjour la famille ca va ? moi oui ça va bien merci", "b*l*r*c") );
+	printf("%d\n", match("mot mot mot", "mot * mot mot") );
+	printf("%d\n", match("ca va ? bien merci", "*ca v* *a ?* bien m*erci**") );
+	printf("%d\n", match("vvvvvv", "*vv*vvvvv") );
+	printf("\nEXPECTED 1\n");
+	printf("%d\n", match("mot mot mot", "mot* mot mot") );
+	printf("%d\n", match("test", "**t") );
+	printf("%d\n", match("test", "**t**") );
+	printf("%d\n", match("test", "**test**") );
+	printf("%d\n", match("test", "t**t**") );
+	printf("%d\n", match("test", "***") );
+	printf("%d\n", match("test", "t**e**") );
+	printf("%d\n", match("", "") );
+	printf("%d\n", match("abc", "abc") );
+	printf("%d\n", match("test", "*t") );
+	printf("%d\n", match("test", "t*") );
+	printf("%d\n", match("salut ca va", "salut c*va") );
+	printf("%d\n", match("plop", "*") );
+	printf("%d\n", match("", "*") );
+	printf("%d\n", match("*", "*") );
+	printf("%d\n", match("plop*p", "*") );
+	printf("%d\n", match("test", "****") );
+	printf("%d\n", match("salut les amis", "salut les*mis") );
+	printf("%d\n", match("caca boudin", "*n") );
+	printf("%d\n", match("cacan boudin", "*n") );
+	printf("%d\n", match("*etoile*", "*etoile*") );
+	printf("%d\n", match("*etoile*", "*etoil*") );
+	printf("%d\n", match("*etoile*", "**etoile*") );
+	printf("%d\n", match("lo*l", "*l") );
+	printf("%d\n", match("lo*l", "l*") );
+	printf("%d\n", match("*", "***") );
+	printf("%d\n", match("***", "*") );
+	printf("%d\n", match("bonjour la famille ça va ? moi oui ça va bien merci", "b*l*r*") );
+	printf("%d\n", match("ca va ? bien merci", "ca* bien merci") );
+	printf("%d\n", match("ca va ? bien merci", "*ca v** ?* bien m*erci**") );
+	printf("%d\n", match("vvvvvv", "*vv*vvvv") );
+	printf("%d\n", match("** salut*jevais bien**et toi ?", "*salut*jevais*bien*i ?") );
+	printf("%d\n", match("**   ere salut*jevais bien*et toi ?", "*salut*jevais*bien*i ?") );
+	return 0;
 }
--- matchnmatch/ex00/match.c
+++ matchnmatch/ex00/match.c
@@ -6,19 +6,46 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/15 08:34:37 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/15 12:45:11 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/07/16 21:48:56 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int		ft_etoile(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (*str)
+	{
+		if (*str == '*')
+			i++;
+		str++;
+	}
+	return (i);
+}
+
 int		match(char *s1, char *s2)
 {
+	//printf("len s1 : %d\nlen s2 : %d\n", ft_strlen(s1), ft_strlen(s2)); 
 	if (*s2 == 0)
+		return (0);
+	if ((ft_strlen(s1) > ft_strlen(s2)) && ft_etoile(s2) < 1)
 		return (0);
 	if (*s1 == 0 && *s2 != '*')
 		return (0);
 	while (*s1 && *s2)
 	{
-		if (*s2 == '*')
+		if (*s2 == '*' && *(s2 - 1) != ' ')
 		{
 			s2++;
 			if (*s2 == 0)
@@ -27,6 +54,8 @@ int		match(char *s1, char *s2)
 			{
 				if (*s1 == 0)
 					return (0);
+				if (*s2 == '*')
+					s2++;
 				else
 					s1++;
 			}
@@ -36,5 +65,7 @@ int		match(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
+	if (*s1 != *s2)
+		return (0);
 	return (1);
 }
